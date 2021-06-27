831b8d7feed2b09e0d4ef94a6ccb0fa731e326e8
liis.kilp <kilp.liis@gmail.com>
2021-06-27 11:15:50 +0200

    fix jammming image and title

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -35,9 +35,9 @@ const Projects = () => {
             title: 'Memory',
         },
         {
-            name: 'jamming',
+            name: 'jammming',
             url: 'https://lkilp.github.io/jammming/',
-            title: 'Jamming',
+            title: 'Jammming',
         },
         {
             name: 'fylo',
@@ -67,48 +67,7 @@ const Projects = () => {
                         <div className='projectLink'>{title}</div>
                     </a> )
                 })}
-                {/* <a className='project crowdfund' target='_blank' rel='noreferrer' href='https://lkilp.github.io/crowdfund/'>
-                    <div className='background'></div>
-                    <div className='projectLink'>Crowdfund</div>
-                </a>
-                <a className='project quiz' target='_blank' rel='noreferrer' href='https://lkilp.github.io/quiz/'>
-                    <div className='background'></div>
-                    <div className='projectLink'>Dutch quiz</div>
-                </a>
-                <a className='project leaf' target='_blank' rel='noreferrer' href='https://lkilp.github.io/leaf/'>
-                    <div className='background'></div>
-                    <div className='projectLink'>Leaf</div>
-                </a>
-                <a className='project blogr' target='_blank' rel='noreferrer' href='https://lkilp.github.io/blogr/'>
-                    <div className='background'></div>
-                    <div className='projectLink'>Blogr</div>
-                </a>
-                <a className='project huddle' target='_blank' rel='noreferrer' href='https://lkilp.github.io/huddle/'>
-                    <div className='background'></div>
-                    <div className='projectLink'>Huddle</div>
-                </a>
-                <a className='project memory' target='_blank' rel='noreferrer' href='https://lkilp.github.io/memory/'>
-                    <div className='background'></div>
-                    <div className='projectLink'>Memory</div>
-                </a>
-                <a className='project jammming' target='_blank' rel='noreferrer' href='https://lkilp.github.io/jammming/'>
-                    <div className='background'></div>
-                    <div className='projectLink'>Jammming</div>
-                </a>
-                <a className='project fylo' target='_blank' rel='noreferrer' href='https://lkilp.github.io/fylo/'>
-                    <div className='background'></div>
-                    <div className='projectLink'>Fylo</div>
-                </a>
-                <a className='project pricing-component' target='_blank' rel='noreferrer' href='https://lkilp.github.io/pricing-component/'>
-                    <div className='background'></div>
-                    <div className='projectLink'>Pricing component</div>
-                </a>
-                <a className='project swimster' target='_blank' rel='noreferrer' href='https://lkilp.github.io/swimster/'>
-                    <div className='background'></div>
-                    <div className='projectLink'>Swimster</div>
-                </a> */}
             </div>
-            
         </div>
     )
 }
